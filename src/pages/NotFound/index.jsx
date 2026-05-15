fe9d5cec35391902fda48f021dfbfa6c17dafb7c
alexalves011 <alexalves.work@gmail.com>
2026-05-15 15:51:17 -0300

    Implementando Layouts página 404

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,32 @@
+import styles from "./notFound.module.css";
+import urso from "../../assets/404-urso.png";
+import Typography from "../../components/Typography";
+import { Link } from "react-router"
+
+export const NotFound = () => {
+  return (
+    <main className={styles.main}>
+
+      <div className={styles.conteudo}>
+
+        <Typography variant="h1" color="--offwhite" className={styles.texto}>
+          404- Not Found
+        </Typography>
+
+        <img src={urso} alt="Urso" className={styles.img}></img>
+
+
+        <Typography variant="p" color="--offwhite" className={styles.texto}>
+            Não conseguimos encontrar a página que você estava procurando.
+            <br/>
+            Que tal explorar nossas Foemações
+        </Typography>
+
+
+        <Link to='/' className={styles.link}>volta para o início</Link>
+
+
+      </div>
+    </main>
+  );
+};
